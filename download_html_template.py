fdc32a7252205047a6e264a38681306a0015c346
maths354 <peltiermathias996@gmail.com>
2024-01-17 12:45:11 +0100

    Ajout d'un tri des balises

--- download_html_template.py
+++ download_html_template.py
@@ -1,6 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
 
-def get_html_content(url):
+def get_html_structure(url):
     try:
         # Effectue une requête GET à l'URL fournie
         response = requests.get(url)
@@ -9,7 +10,14 @@ def get_html_content(url):
         if response.status_code == 200:
             # Récupère le contenu HTML de la page
             html_content = response.text
-            return html_content
+            
+            # Utilise BeautifulSoup pour analyser le HTML
+            soup = BeautifulSoup(html_content, 'html.parser')
+            
+            # Extrait toutes les balises sans leur contenu
+            html_structure = '\n'.join(tag.prettify(formatter=None) for tag in soup.find_all(True, recursive=False))
+            
+            return html_structure
         else:
             print(f"La requête a échoué avec le code d'état : {response.status_code}")
     except Exception as e:
@@ -18,7 +26,7 @@ def get_html_content(url):
 # Demande à l'utilisateur de fournir l'URL
 url = input("Entrez l'URL de la page dont vous souhaitez récupérer le code HTML : ")
 
-# Appelle la fonction et affiche le code HTML si disponible
-html_content = get_html_content(url)
-if html_content:
-    print(html_content)
+# Appelle la fonction et affiche la structure HTML si disponible
+html_structure = get_html_structure(url)
+if html_structure:
+    print(html_structure)
