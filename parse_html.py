46a9d51610f1f0b9c8f5cddf553758df6581d822
nnweg <gwendal.sln@gmail.com>
2024-02-07 16:26:56 +0100

    Added comment

--- parse_html.py
+++ parse_html.py
@@ -1,17 +1,23 @@
 import re
 html = '<html><head><h1>Hi</h1><p>test <span class="time">test</span></p></head></html>'
+
+#using re library to get all tags
 tags = re.findall(r'<[^>]+>', html)
 
-string=""
+#String variable used to concatenate parsed tag
+parsed_tag=""
 
-for a in tags:
-    if " " in a:
-        a = a.replace(a,a.split(" ")[0])
-    if "/" not in a:
-        a=a.split("<")[1].split(">")[0]
-        string+=f"{a}+("
+#Check every tag
+for tag in tags:
+    #condition used to get only the start of the tag, for example "span" from <span class="time">
+    if " " in tag:
+        tag = tag.replace(tag,tag.split(" ")[0])
+    #condition used to get only start tag like <span>
+    if "/" not in tag:
+        tag=tag.split("<")[1].split(">")[0]
+        parsed_tag+=f"{tag}+("
+    #condition used to get only end tag like </span>
     else:
-        string+=")"
-        #print(a)
+        parsed_tag+=")"
 
-print(string)
+print(parsed_tag)
