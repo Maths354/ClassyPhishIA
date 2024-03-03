1743e0398aa4466ff0be2a8ed6d8926daf2841b3
maths354 <peltiermathias996@gmail.com>
2024-03-03 10:19:15 +0100

    ajout d'un user-agent + retour d'erreur 403

--- get_template/extract_url.py
+++ get_template/extract_url.py
@@ -3,8 +3,14 @@ from bs4 import BeautifulSoup
 import re
 
 def extract_and_save_urls(url):
-    # Faire la requête HTTP pour obtenir le contenu de la page web
-    response = requests.get(url)
+    # Définir un en-tête User-Agent
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+
+    # Faire la requête HTTP avec l'en-tête User-Agent
+    response = requests.get(url, headers=headers)
+    
     # Vérifier si la requête a réussi
     if response.status_code == 200:
         # Utiliser BeautifulSoup pour analyser le contenu HTML
@@ -20,15 +26,18 @@ def extract_and_save_urls(url):
         # Enregistrer les URL dans les fichiers
         with open('all_url.txt', 'w') as non_url_file:
             for non_url in non_url_list:
-                non_url_file.write(non_url + '\n')
+                non_url_file.write(str(non_url) + '\n')
 
         with open('url.txt', 'w') as url_file:
             for regex_url in regex_urls:
-                url_file.write(regex_url + '\n')
+                url_file.write(str(regex_url) + '\n')
 
         return non_url_list, regex_urls
+    elif response.status_code == 403:
+        print("Accès refusé. Veuillez vérifier vos permissions.")
+        return [], []
     else:
-        # Afficher un message d'erreur si la requête a échoué
+        # Afficher un message d'erreur si la requête a échoué pour une autre raison
         print("La requête a échoué avec le code de statut:", response.status_code)
         return [], []
 
