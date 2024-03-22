85e9d84492cf3802d4233bf5658ed9f1d86b5063
nnweg <gwendal.sln@gmail.com>
2024-03-22 09:44:12 +0100

    fichier passer en fonction 'parse_html_string'

--- parse_html.py
+++ parse_html.py
@@ -1,23 +1,21 @@
 import re
-html = '<html><head><h1>Hi</h1><p>test <span class="time">test</span></p></head></html>'
 
-#using re library to get all tags
-tags = re.findall(r'<[^>]+>', html)
+def parse_html_string(html_string):
+    #using re library to get all tags
+    tags = re.findall(r'<[^>]+>', html_string)
 
-#String variable used to concatenate parsed tag
-parsed_tag=""
+    #String variable used to concatenate parsed tag
+    parsed_tag=""
 
-#Check every tag
-for tag in tags:
-    #condition used to get only the start of the tag, for example "span" from <span class="time">
-    if " " in tag:
-        tag = tag.replace(tag,tag.split(" ")[0])
-    #condition used to get only start tag like <span>
-    if "/" not in tag:
-        tag=tag.split("<")[1].split(">")[0]
-        parsed_tag+=f"{tag}+("
-    #condition used to get only end tag like </span>
-    else:
-        parsed_tag+=")"
-
-print(parsed_tag)
+    #Check every tag
+    for tag in tags:
+        #condition used to get only the start of the tag, for example "span" from <span class="time">
+        if " " in tag:
+            tag = tag.replace(tag,tag.split(" ")[0])
+        #condition used to get only start tag like <span>
+        if "/" not in tag:
+            tag=tag.split("<")[1].split(">")[0]
+            parsed_tag+=f"{tag}+("
+        #condition used to get only end tag like </span>
+        else:
+            parsed_tag+=")"
