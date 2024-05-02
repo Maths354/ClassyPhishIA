b553dc3730b2de9c814d24de93e475f9e7a16f01
maths354 <peltiermathias996@gmail.com>
2024-05-02 18:56:28 +0200

    suppression scripts inutiles

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from get_template.get_template import url_input
 from get_logo.get_logo import logo_input
-enter_url = url_input(input_data="https://www.keraunos.org/")
-# enter_logo = logo_input(url="https://www.keraunos.org/")
+# enter_url = url_input(input_data="https://orangesys.pro/")
+enter_logo = logo_input(url="https://www.itaqmix.com.br/")
