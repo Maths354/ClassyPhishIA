b8e0f5f64ce60b30c371649a67d15641ba762328
maths354 <peltiermathias996@gmail.com>
2024-05-08 09:18:30 +0200

    maj main

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from get_template.get_template import url_input
 from get_logo.get_logo import logo_input
 
-# url1 = "https://messagerie-mobile-orange.click-app.icu/"
-# url2 = "https://login.orange.fr/?service=nextecare&return_url=https://espace-client.orange.fr/page-accueil"
-# enter_url = url_input(url1,url2)
-# enter_logo = logo_input(url="https://www.orange.fr/portail")
-
 url2 = "https://messagerie-mobile-orange.click-app.icu/"
 url1 = "https://www.keraunos.org/"
 
+enter_url = url_input(url1,url2)
 enter_logo = logo_input(url1,url2)
