8fa54292ee7821b9578c7d27954a457e7bb021c4
maths354 <peltiermathias996@gmail.com>
2024-05-26 11:36:01 +0200

    ajout vérif image

--- analyse_phishing/extract_logo/extract_logo.py
+++ analyse_phishing/extract_logo/extract_logo.py
@@ -8,6 +8,8 @@ import cv2
 import numpy as np
 from skimage.metrics import structural_similarity as ssim
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 class ExtractLOGO:
     def __init__(self, url=None):
         self.url = url
@@ -57,11 +59,16 @@ class ExtractLOGO:
 
             filename = os.path.join(directory, f"{data_id}.png")
 
-            response = requests.get(image_url)
+            response = requests.get(image_url, timeout=10)
             response.raise_for_status()
 
             with open(filename, 'wb') as file:
                 file.write(response.content)
+
+            # Vérifiez si l'image est correctement téléchargée et lisible
+            if cv2.imread(filename) is None:
+                logging.error(f"Le fichier téléchargé à partir de {image_url} n'est pas une image valide")
+                return None, None
 
             sha256_hash = hashlib.sha256()
 
@@ -93,9 +100,13 @@ class ExtractLOGO:
     def load_image(self, file_path):
         try:
             image = cv2.imread(file_path)
+            if image is None:
+                logging.error(f"Impossible de charger l'image à partir de {file_path}")
+                return None
             gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             return gray_image
-        except:
+        except Exception as e:
+            logging.error(f"Erreur lors du chargement de l'image : {e}")
             return None
 
     def resize_image(self, image, size):
@@ -109,16 +120,20 @@ class ExtractLOGO:
         similarity, _ = ssim(resized_image_legitime, resized_image_phishing, full=True)
         return similarity
 
-    def logo_info(self, url_legitime="https://www.orange.fr",):
+    def logo_info(self, url_legitime="https://www.orange.fr"):
         image_path_legitime, hash_legitime = self.process_url(url_legitime)
         image_path_phishing, hash_phishing = self.process_url(self.url)
+
+        similarity_score = None
 
         if image_path_legitime and image_path_phishing:
             image_legitime = self.load_image(image_path_legitime)
             image_phishing = self.load_image(image_path_phishing)
-            similarity_score = self.compare_images(image_legitime, image_phishing)
-            logging.info(f"Score de ressemblance entre les images : {similarity_score:.2f}")
-            logging.info(f"SHA-256 de l'image 1 : {hash_legitime}")
-            logging.info(f"SHA-256 de l'image 2 : {hash_phishing}")
+
+            if image_legitime is not None and image_phishing is not None:
+                similarity_score = self.compare_images(image_legitime, image_phishing)
+                logging.info(f"Score de ressemblance entre les images : {similarity_score:.2f}")
+                logging.info(f"SHA-256 de l'image 1 : {hash_legitime}")
+                logging.info(f"SHA-256 de l'image 2 : {hash_phishing}")
 
         return hash_phishing, similarity_score
