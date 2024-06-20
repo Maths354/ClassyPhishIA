0c97dfced2364a912c07fab90dc439c5bedfd43b
Madore224 <112936472+Madore224@users.noreply.github.com>
2024-06-20 16:30:15 +0200

    Update model.py

--- analyse_phishing/model/model.py
+++ analyse_phishing/model/model.py
@@ -5,9 +5,8 @@ from sklearn.metrics import accuracy_score # type: ignore
 
 class Model:
     
-    def __init__(self, score_url, score_text, score_logo, score_cert):
+    def __init__(self, score_url, score_logo, score_cert):
         self.score_url = score_url
-        self.score_text = score_text
         self.score_logo = score_logo
         self.score_cert = score_cert
 
@@ -18,10 +17,9 @@ class Model:
 
         # Site de phishing à tester
         new_site_features = {
-            'url_score': [0.00],
-            'text_score': [0.00],
-            'logo_similarity': [0.90],
-            # 'cert_valid': [1]
+            'url_score': [self.score_url],
+            'logo_similarity': [self.score_logo],
+            'cert_valid': [self.score_cert]
         }
 
         new_site_df = pd.DataFrame(new_site_features)
@@ -30,7 +28,8 @@ class Model:
         predicted_site_proba = logistic_model.predict_proba(new_site_df)
         #print(f'The proba is : {predicted_site_proba[0][1]}')
 
-        return float("%.4f" % predicted_site_proba[0][1])
+        print("%.3f" % predicted_site_proba[0][1])
+        return float("%.3f" % predicted_site_proba[0][1])
         # Récupérer le nom du site légitime à partir de l'ID (supposons que `site_id_to_name` est un dictionnaire que vous avez créé)
         # site_name = site_id_to_name[predicted_site_id[0]]
         # print(f'The site is impersonating: {site_name}')
@@ -40,17 +39,18 @@ class Model:
 
         # Données fictives d'entrainement à récupérer en BDD_SCORE.
         data = {
-            'url_score':        [0.95, 0.80, 0.60, 0.40, 0.30, 0.35, 0.15, 0.05, 0.90, 0.20, 0.05, 0.00, 1.00, 0.00, 0.00, 1.00, 0.00],
-            'text_score':       [0.90, 0.65, 0.55, 0.30, 0.25, 0.58, 0.20, 0.20, 0.62, 0.10, 0.20, 0.00, 1.00, 0.50, 0.00, 1.00, 1.00],
-            'logo_similarity':  [0.98, 0.10, 0.50, 0.15, 0.20, 0.99, 0.99, 0.05, 0.10, 0.33, 0.10, 0.00, 1.00, 0.99, 1.00, 1.00, 1.00],
-            # 'cert_valid':       [1, 0, 0, 1, 0],
-            'site_identity':    [1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1]  # Identité réelle du site (label)
+            'url_score':        [1.00, 0.80, 0.60, 0.35, 0.15, 0.90, 0.20, 0.00, 1.00, 0.80, 0.00, 1.00, 0.30, 1.00, 0.87],
+            #'text_score':       [0.90, 0.65, 0.55, 0.25, 0.58, 0.20, 0.20, 0.62, 0.10, 0.20, 0.00, 1.00, 0.40, 0.00, 0.40, 1.00],
+            'logo_similarity':  [1.00, 0.10, 0.50, 1.00, 1.00, 1.00, 0.33, 0.00, 1.00, 1.00, 1.00, 1.00, 1.00, 0.50, 0.10],
+            'cert_valid':       [0.80, 0.80, 0.85, 0.40, 0.30, 0.90, 0.05, 0.00, 1.00, 0.90, 0.50, 0.95, 0.90, 1.00, 1.00],
+            'site_identity':    [1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1]  # Identité réelle du site (label)
         }
 
         df = pd.DataFrame(data)
 
         #X = df[['url_score', 'text_sco re', 'logo_similarity', 'has_https', 'cert_valid']]
-        X = df[['url_score', 'text_score', 'logo_similarity']]
+        #X = df[['url_score', 'text_score', 'logo_similarity']]
+        X = df[['url_score', 'logo_similarity', 'cert_valid']]
         y = df['site_identity']
 
         # Séparation des données en ensembles d'entraînement et de test
