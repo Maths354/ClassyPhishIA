ba00e1e6c1e3ec459e6862693825b479d282a1ad
Madore224 <112936472+Madore224@users.noreply.github.com>
2024-06-20 11:00:36 +0200

    Update loading.js

--- apps/static/js/loading.js
+++ apps/static/js/loading.js
@@ -5,5 +5,5 @@ document.getElementById('loading').addEventListener('click', function() {
     overlay.style.display = 'flex';
     setTimeout(function() {
         overlay.style.display = 'none';
-    }, 5000);
+    }, 12000);
 });
