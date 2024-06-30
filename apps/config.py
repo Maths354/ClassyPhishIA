79a8ddf0d8fc687e5a64bdf7ce8b4ff5b6c17e0e
nnweg <gwendal.sln@gmail.com>
2024-06-30 20:10:51 +0200

    random secret key

--- apps/config.py
+++ apps/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     DEBUG = True
     DEVELOPMENT = True
-    SECRET_KEY = 'secret_key_test'
+    SECRET_KEY = os.urandom(24)
     FLASK_SECRET = SECRET_KEY
     SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
