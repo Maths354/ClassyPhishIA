38cd808f60538169eccb0a17a0594bb7a787625f
maths354 <peltiermathias996@gmail.com>
2024-07-02 10:36:08 +0200

    suppression de deux points inutiles

--- analyse_phishing/model/tab_positives_negatives.py
+++ analyse_phishing/model/tab_positives_negatives.py
@@ -105,37 +105,6 @@ class URLAnalyzer:
         except Exception as e:
             self.negative_points.append(f"Erreur lors de la vérification de la réputation du domaine : {str(e)}")
 
-    def check_content_security(self):
-        """Vérifie les headers de sécurité HTTP."""
-        try:
-            response = requests.head(self.url, timeout=5)
-            headers = response.headers
-            security_headers = ["Content-Security-Policy", "Strict-Transport-Security"]
-            missing_headers = [header for header in security_headers if header not in headers]
-            if not missing_headers:
-                self.positive_points.append("Les headers de sécurité essentiels sont présents.")
-            else:
-                self.negative_points.append(f"Les headers de sécurité suivants sont manquants : {', '.join(missing_headers)}.")
-        except Exception as e:
-            self.negative_points.append(f"Erreur lors de la vérification des headers de sécurité HTTP : {str(e)}")
-
-    def check_privacy_policy(self):
-        """Vérifie la présence de la politique de confidentialité et des mentions légales."""
-        try:
-            response = requests.get(self.url, timeout=5)
-            content = response.text.lower()
-            if "politique de confidentialité" in content or "privacy policy" in content:
-                self.positive_points.append("La politique de confidentialité est présente.")
-            else:
-                self.negative_points.append("La politique de confidentialité n'est pas trouvée.")
-            
-            if "mentions légales" in content or "legal notice" in content:
-                self.positive_points.append("Les mentions légales sont présentes.")
-            else:
-                self.negative_points.append("Les mentions légales ne sont pas trouvées.")
-        except Exception as e:
-            self.negative_points.append(f"Erreur lors de la vérification de la politique de confidentialité et des mentions légales : {str(e)}")
-
     def check_user_reviews(self):
         """Vérifie la réputation du site sur des plateformes d'avis."""
         try:
@@ -185,8 +154,6 @@ class URLAnalyzer:
         self.check_server_location()
         self.check_whois_visibility()
         self.check_domain_reputation()
-        self.check_content_security()
-        self.check_privacy_policy()
         self.check_user_reviews()
         self.check_social_media_activity()
         self.check_page_speed()
